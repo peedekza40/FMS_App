a9714c671f4e22b56177b54e3a688163c941a0b2
“peedekza40” <59160214@go.buu.ac.th>
2019-05-05 03:07:33 +0700

    create service api

--- app/src/main/java/com/example/fms_app/Service/Income.kt
+++ app/src/main/java/com/example/fms_app/Service/Income.kt
@@ -1,28 +1,69 @@
 package com.example.fms_app.Service
 
 import android.content.Context
+import android.os.Build
+import android.support.annotation.RequiresApi
+import android.widget.Toast
+import com.android.volley.Request
 import com.android.volley.RequestQueue
-import com.android.volley.toolbox.BasicNetwork
-import com.android.volley.toolbox.DiskBasedCache
-import com.android.volley.toolbox.HurlStack
-import com.android.volley.toolbox.Volley
+import com.android.volley.Response
+import com.android.volley.toolbox.*
+import java.io.File
+import kotlin.collections.ArrayList
+import org.json.JSONObject
 
-class Income (
+
+
+class Income(
     val context: Context,
-    val cacheDir
-){
-    private var requestQueue: RequestQueue? = null
-    constructor(){
-        // Instantiate the cache
-        val cache = DiskBasedCache(cacheDir, 1024 * 1024) // 1MB cap
-        // Set up the network to use HttpURLConnection as the HTTP client.
-        val network = BasicNetwork(HurlStack())
+    val cacheDir: File) {
 
-        // Instantiate the RequestQueue with the cache and network. Start the queue.
+    private var requestQueue: RequestQueue? = null
+    private var ip: String = "http://192.168.1.3:80"
+    private var url: String = ip
+    init {
+        val cache = DiskBasedCache(cacheDir, 1024 * 1024) // 1MB cap
+        val network = BasicNetwork(HurlStack())
         requestQueue = RequestQueue(cache, network).apply {
             start()
         }
-//        // Instantiate the RequestQueue.
         requestQueue = Volley.newRequestQueue(context)
     }
+
+    fun get_all(callback: VolleyCallback){
+        url += "/get_all_income"
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                //val accounting = JSONArray(response)
+                callback.onSuccess(response)
+            },
+            Response.ErrorListener {
+                    response-> Toast.makeText(context, "${response}", Toast.LENGTH_SHORT).show()
+            }
+        )
+        stringRequest.tag = "Service FMS"
+        // Add the request to the RequestQueue.
+        requestQueue?.add(stringRequest)
+    }
+
+    fun insert(callback: VolleyCallback){
+        url += "/insert_income"
+        val jsonBody = JSONObject()
+        //jsonBody.put("")
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.POST, url,
+            Response.Listener<String> { response ->
+                //val accounting = JSONArray(response)
+                callback.onSuccess(response)
+            },
+            Response.ErrorListener {
+                    response-> Toast.makeText(context, "${response}", Toast.LENGTH_SHORT).show()
+            }
+        )
+    }
+
+
 }
---
+++ app/src/main/java/com/example/fms_app/Service/VolleyCallback.kt
@@ -0,0 +1,5 @@
+package com.example.fms_app.Service
+
+interface VolleyCallback {
+    fun onSuccess(result: String)
+}
