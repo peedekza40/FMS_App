2284858fc15f02e028e330bd92dcbc7c853f0258
“peedekza40” <59160214@go.buu.ac.th>
2019-05-04 15:44:54 +0700

    add service volley

--- app/src/main/java/com/example/fms_app/Add_accounting.kt
+++ app/src/main/java/com/example/fms_app/Add_accounting.kt
@@ -6,9 +6,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.NavUtils
 import android.view.MenuItem
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.*
+import org.json.JSONArray
+import kotlinx.android.synthetic.main.activity_add_accounting.*
 
 class Add_accounting : AppCompatActivity() {
-
+    var requestQueue: RequestQueue? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_accounting)
@@ -19,7 +25,37 @@ class Add_accounting : AppCompatActivity() {
         actionbar!!.title = "เพิ่มรายการบัญชี"
         //set back button
         actionbar.setDisplayHomeAsUpEnabled(true)
-        //sadasdasdda
+
+// Instantiate the cache
+        val cache = DiskBasedCache(cacheDir, 1024 * 1024) // 1MB cap
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        val network = BasicNetwork(HurlStack())
+
+        // Instantiate the RequestQueue with the cache and network. Start the queue.
+        requestQueue = RequestQueue(cache, network).apply {
+            start()
+        }
+//        // Instantiate the RequestQueue.
+        requestQueue = Volley.newRequestQueue(this)
+
+        val url = "http://10.80.84.129:80/get_all_income"
+
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                var json = JSONArray(response)
+                // Display the first 500 characters of the response string.
+                codeText.text = "Response is: ${json.getJSONObject(0).getString("inc_code")}"
+            },
+            Response.ErrorListener {
+                    response-> codeText.text = "${response}"
+            }
+        )
+
+        stringRequest.tag = "Service FMS"
+        // Add the request to the RequestQueue.
+        requestQueue?.add(stringRequest)
     }
 
 
