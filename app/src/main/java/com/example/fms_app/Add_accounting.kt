42fc6315765caea57f9b7ef5e07541c0cd1e3f77
“peedekza40” <59160214@go.buu.ac.th>
2019-05-07 00:10:44 +0700

    make tap and form add accounting

--- app/src/main/java/com/example/fms_app/Add_accounting.kt
+++ app/src/main/java/com/example/fms_app/Add_accounting.kt
@@ -17,38 +17,61 @@ import com.example.fms_app.Service.Income
 import com.example.fms_app.Service.VolleyCallback
 import org.json.JSONArray
 import kotlinx.android.synthetic.main.activity_add_accounting.*
+import android.support.v4.view.ViewPager
+import android.support.design.widget.TabLayout
+import android.widget.EditText
+
 
 class Add_accounting : AppCompatActivity() {
+
     var requestQueue: RequestQueue? = null
-    @RequiresApi(Build.VERSION_CODES.O)
+    private var tapAdapter: TabAdapter? = null
+    private var tabLayout: TabLayout? = null
+    private var viewPager: ViewPager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_accounting)
 
+        /*-----------------set action bar-------------------*/
         //actionbar
         val actionbar = supportActionBar
         //set actionbar title
         actionbar!!.title = "เพิ่มรายการบัญชี"
         //set back button
         actionbar.setDisplayHomeAsUpEnabled(true)
+
+        /*-----------------use service-------------------*/
         val service_income = Income(this,cacheDir)
         service_income.get_all(object : VolleyCallback {
             override fun onSuccess(result: String) {
                 //codeText.text = JSONArray(result).getJSONObject(0).getString("inc_code")
             }
         })
+
+        /*-----------------set tap-------------------*/
+        viewPager = findViewById(R.id.viewPager)
+        tabLayout = findViewById(R.id.tabLayout)
+        tapAdapter = TabAdapter(supportFragmentManager)
+
+        tapAdapter!!.addFragment(add_income(), "รายรับ")
+        tapAdapter!!.addFragment(add_payment(), "รายจ่าย")
+
+        viewPager!!.setAdapter(tapAdapter)
+        tabLayout!!.setupWithViewPager(viewPager)
+
     }
 
 
 
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when (item!!.itemId) {
-            android.R.id.home -> {
-                finish()
-                return true
-            }
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
+//    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+//        when (item!!.itemId) {
+//            android.R.id.home -> {
+//                finish()
+//                return true
+//            }
+//            else -> return super.onOptionsItemSelected(item)
+//        }
+//    }
 }
