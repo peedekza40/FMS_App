613cfbb2be58aeccc0a4943d1fc72d35481b89bf
“peedekza40” <59160214@go.buu.ac.th>
2019-05-14 06:43:08 +0700

    Merge remote-tracking branch 'remotes/origin/Auh_V2' into Assemble_1
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/example/fms_app/Manage_base_data.kt
    #	app/src/main/java/com/example/fms_app/Service/Description.kt
    #	app/src/main/java/com/example/fms_app/Service/Income.kt
    #	app/src/main/java/com/example/fms_app/add_income.kt

---
+++ app/src/main/java/com/example/fms_app/Service/Payment.kt
@@ -0,0 +1,85 @@
+package com.example.fms_app.Service
+
+import android.content.Context
+import android.os.Build
+import android.support.annotation.RequiresApi
+import android.util.Log
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.*
+import java.io.File
+import kotlin.collections.ArrayList
+import org.json.JSONObject
+import com.example.fms_app.Service.Conifg_service
+
+
+
+class Payment(
+    val context: Context,
+    val cacheDir: File): Conifg_service(context, cacheDir){
+
+    init {
+    }
+
+    fun get_all(callback: VolleyCallback){
+        url += "/get_all_payment"
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                //val accounting = JSONArray(response)
+                callback.onSuccess(response)
+            },
+            Response.ErrorListener {
+                    response-> Log.d("Service error",response.toString())
+            }
+        )
+        stringRequest.tag = "Service FMS"
+        // Add the request to the RequestQueue.
+        requestQueue?.add(stringRequest)
+    }
+
+    fun get_last_pay_code(callback: VolleyCallback){
+        url += "/get_last_pay_code"
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                callback.onSuccess(response)
+            },
+            Response.ErrorListener {
+                    response-> Log.d("Service error",response.toString())
+            }
+        )
+        stringRequest.tag = "Service FMS"
+        // Add the request to the RequestQueue.
+        requestQueue?.add(stringRequest)
+    }
+
+    fun insert(callback: VolleyCallback){
+        url += "/insert_payment"
+        val jsonBody = JSONObject()
+        //jsonBody.put("")
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.POST, url,
+            Response.Listener<String> { response ->
+                //val accounting = JSONArray(response)
+                callback.onSuccess(response)
+            },
+            Response.ErrorListener {
+                    response-> Toast.makeText(context, "${response}", Toast.LENGTH_SHORT).show()
+            }
+        )
+    }
+
+    fun getByRangeDate(callback: VolleyCallback){
+        url += "/get_by_pay_bac_id_and_date"
+        val jsonBody = JSONObject()
+
+    }
+
+
+}
