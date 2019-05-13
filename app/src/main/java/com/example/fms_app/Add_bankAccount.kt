661bf182e8055f2e8fc3e5f63e621d981139ef0f
59160215 <59160215@go.buu.ac.th>
2019-05-14 05:37:17 +0700

    show data with service Bank_account 3
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/fms_app/Add_bankAccount.kt
+++ app/src/main/java/com/example/fms_app/Add_bankAccount.kt
@@ -3,6 +3,7 @@ package com.example.fms_app
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_add_bank_account.*
 
 class Add_bankAccount : AppCompatActivity() {
@@ -13,7 +14,21 @@ class Add_bankAccount : AppCompatActivity() {
         val actionBar = supportActionBar
         actionBar!!.title = "เพิ่มข้อมูลบัญชีเงินฝาก"
         actionBar.setDisplayHomeAsUpEnabled(true)
+        var input_bacId = findViewById<EditText>(R.id.text_bacId)
+        var input_bacNo = findViewById<EditText>(R.id.text_bacNo)
+        var input_zbank = findViewById<EditText>(R.id.text_Zbank)
+        var input_bacName = findViewById<EditText>(R.id.text_bacName)
+        var input_batId = findViewById<EditText>(R.id.text_batId)
+        var input_baId = findViewById<EditText>(R.id.text_baId)
+        var input_balance = findViewById<EditText>(R.id.text_balance)
         save_btn.setOnClickListener {
+            var bacId =input_bacId.text.toString()
+            var bacNo =input_bacNo.text.toString()
+            var zbank =input_zbank.text.toString()
+            var bacName =input_bacName.text.toString()
+            var batId =input_batId.text.toString()
+            var baId =input_baId.text.toString()
+            var balance =input_balance.text.toString()
             finish()
         }
     }
