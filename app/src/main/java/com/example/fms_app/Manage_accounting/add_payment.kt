a749260ec7ff5b8fbd96f33e61e3b9b292df25d0
“peedekza40” <59160214@go.buu.ac.th>
2019-05-15 03:14:09 +0700

    Merge remote-tracking branch 'remotes/origin/Auh_V2' into Assemble_3
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/java/com/example/fms_app/Service/Income.kt

--- app/src/main/java/com/example/fms_app/Manage_accounting/add_payment.kt
+++ app/src/main/java/com/example/fms_app/Manage_accounting/add_payment.kt
@@ -1,12 +1,31 @@
 package com.example.fms_app.Manage_accounting
 
+import android.app.DatePickerDialog
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.v4.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Toast
+import com.example.fms_app.Data_class.BankAccount
+import com.example.fms_app.Data_class.Desc
 import com.example.fms_app.R
+import com.example.fms_app.Service.*
+import kotlinx.android.synthetic.main.fragment_add_income.*
+import org.json.JSONArray
+import org.json.JSONObject
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -43,6 +62,229 @@ class add_payment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_payment, container, false)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        //service
+        val service_payment = Payment(activity!!.applicationContext,activity!!.cacheDir)
+        val service_bankACC = Bank_account(activity!!.applicationContext, activity!!.cacheDir)
+        val service_desc = Description(activity!!.applicationContext, activity!!.cacheDir)
+        //Data class
+
+
+
+        /*----------------------set pay_code------------------------------------*/
+        service_payment.get_last_pay_code(object : VolleyCallback {
+            override fun onSuccess(result: JSONObject) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onSuccess(result: String) {
+                val last_code = JSONArray(result).getJSONObject(0).getString("pay_code")
+                codeText.setText(gen_pay_code(last_code))
+            }
+        })
+        /*---------------------set datepicker-------------------------*/
+        this.setDatePicker(dateText)//inc_date
+        this.setDatePicker(receiptDateText)//inc_receipt_date
+
+        /*---------------------set bac dropdown-------------------------*/
+        bacText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                bacText.setError(null)
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+        })
+
+        service_bankACC.get_bankAccount(object : VolleyCallback {
+            override fun onSuccess(result: JSONObject) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onSuccess(result: String) {
+                val bac_frm_svc = JSONArray(result)
+
+                val bac = ArrayList<BankAccount>()
+
+                (0 until bac_frm_svc.length()).mapTo(bac) {
+                    BankAccount(
+                        bac_frm_svc.getJSONObject(it).getInt("bacId"),
+                        bac_frm_svc.getJSONObject(it).getString("bacName")
+                    )
+                }
+
+                //Create Array Adapter
+                val adapter = ArrayAdapter<BankAccount>(activity, android.R.layout.simple_dropdown_item_1line, bac)
+                bacText.setAdapter(adapter)
+            }
+        })
+        bacText.setOnFocusChangeListener { v, hasFocus ->
+            bacText.showDropDown()
+        }
+        bacText.setOnClickListener {
+            bacText.showDropDown()
+        }
+
+        bacText.setOnItemClickListener(AdapterView.OnItemClickListener { arg0, arg1, arg2, arg3 ->
+            val result = arg0.getItemAtPosition(arg2) as BankAccount
+            bacId.setText(result.bacId.toString())//TextView in layout is visible = gone
+        })
+
+        /*---------------------set desc dropdown-------------------------*/
+        descText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                descText.setError(null)
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+        })
+
+        service_desc.get_desc_by_desctype(2, object : VolleyCallback {
+            override fun onSuccess(result: JSONObject) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onSuccess(result: String) {
+                val desc_frm_srv = JSONArray(result)
+                val desc = ArrayList<Desc>()
+
+                (0 until desc_frm_srv.length()).mapTo(desc) {
+                    Desc(
+                        desc_frm_srv.getJSONObject(it).getInt("desc_id"),
+                        desc_frm_srv.getJSONObject(it).getString("desc_desid"),
+                        desc_frm_srv.getJSONObject(it).getString("desc_description"),
+                        desc_frm_srv.getJSONObject(it).getInt("desc_type")
+                    )
+                }
+                //Create Array Adapter
+                val adapter = ArrayAdapter<Desc>(activity, android.R.layout.simple_dropdown_item_1line, desc)
+                descText.setAdapter(adapter)
+            }
+        })
+        descText.setOnFocusChangeListener { v, hasFocus ->
+            descText.showDropDown()
+        }
+        descText.setOnClickListener {
+            descText.showDropDown()
+        }
+        descText.setOnItemClickListener(AdapterView.OnItemClickListener { arg0, arg1, arg2, arg3 ->
+            val result = arg0.getItemAtPosition(arg2) as Desc
+            descId.setText(result.desc_id.toString())//TextView in layout is visible = gone
+        })
+
+
+
+        /*---------------------click save-------------------------*/
+        saveBtn.setOnClickListener {
+            if(this.validate()){
+                val jsonBody = JSONObject()
+                jsonBody.put("pay_code", codeText.text.toString())
+                jsonBody.put("pay_date", TransformDate(dateText.text.toString()).getDateforDb_1())
+                jsonBody.put("pay_receipt", receiptText.text.toString())
+                jsonBody.put("pay_receipt_code", receiptCodeText.text.toString())
+                jsonBody.put("pay_receipt_date", TransformDate(receiptDateText.text.toString()).getDateforDb_1())
+                jsonBody.put("pay_amount", amountText.text.toString().toDouble())
+                jsonBody.put("pay_detail", "")
+                jsonBody.put("pay_desc_id", descId.text.toString().toInt())
+                jsonBody.put("pay_bac_id", bacId.text.toString().toInt())
+                service_payment.insert(jsonBody, object : VolleyCallback {
+                    override fun onSuccess(result: JSONObject) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                    override fun onSuccess(result: String) {
+                        val status = JSONObject(result).getBoolean("status")
+                        if(status){
+                            Toast.makeText(activity, "เพิ่มรายการบัญชีสำเร็จ", Toast.LENGTH_LONG).show()
+                            activity!!.finish()
+                        }
+                    }
+
+                })
+            }else{
+                Toast.makeText(activity, "กรุณากรอกข้อมูลให้ครบถ้วน", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun validate(): Boolean{
+        val service_bankACC = Bank_account(activity!!.applicationContext, activity!!.cacheDir)
+        var result: Boolean = true
+        if(bacText.text.toString() == ""){
+            bacText.setError( "กรุณาเลือกบัญชีธนาคาร" )
+            result = false
+        }
+        if(amountText.text.toString() == ""){
+            amountText.setError( "กรุณากรอกจำนวนเงิน" )
+            result = false
+        }
+        if(descText.text.toString() == ""){
+            descText.setError("กรุณาเลือกคำอธิบาย")
+            result = false
+        }
+
+        return result
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun setDatePicker(editText: EditText){
+        //for set date EditText
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+
+        //for date picker
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        var date = TransformDate(current.format(formatter)).getThaiDate_1()
+        editText.setText(date)
+
+        editText.setOnClickListener {
+            val dpd = DatePickerDialog(activity, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                // Display Selected date in Toast
+                var dd = dayOfMonth.toString().padStart(2, '0')
+                var mm = (monthOfYear + 1).toString().padStart(2, '0')
+                editText.setText(TransformDate("$dd-$mm-$year").getThaiDate_1())
+            }, year, month, day)
+            dpd.show()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun gen_pay_code(last_code : String) : String{
+        val current = LocalDateTime.now()
+        val year_format = DateTimeFormatter.ofPattern("yyyy")
+        val month_format = DateTimeFormatter.ofPattern("MM")
+        var inc_code = ""
+
+        var year =  (current.format(year_format).toInt() + 543).toString().substring(2)
+        var month = current.format(month_format)
+        var month_code = last_code.substring(4, 6)
+        var year_code = last_code.substring(2, 4)
+
+        if(last_code.equals("") || last_code.equals(null) || (month != month_code) || (year != year_code)){
+            inc_code = "PA" + year + month + "1".padStart(4, '0')
+        }else{
+            var sub_code = (last_code.substring(6).toInt() + 1).toString()
+            inc_code = "PA" + year + month + sub_code.padStart(4, '0')
+        }
+
+        return inc_code
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/com/example/fms_app/Service/Payment.kt
+++ app/src/main/java/com/example/fms_app/Service/Payment.kt
@@ -5,6 +5,7 @@ import android.os.Build
 import android.support.annotation.RequiresApi
 import android.util.Log
 import android.widget.Toast
+import com.android.volley.AuthFailureError
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
@@ -19,12 +20,13 @@ import com.example.fms_app.Service.Conifg_service
 class Payment(
     val context: Context,
     val cacheDir: File): Conifg_service(context, cacheDir){
-
+    val TAG = "Service Pay"
     init {
     }
 
     fun get_all(callback: VolleyCallback){
-        url += "/get_all_payment"
+        url = ip
+        url += "/Payment_service/get_all_payment"
         // Request a string response from the provided URL.
         val stringRequest = StringRequest(
             Request.Method.GET, url,
@@ -36,13 +38,14 @@ class Payment(
                     response-> Log.d("Service error",response.toString())
             }
         )
-        stringRequest.tag = "Service FMS"
+        stringRequest.tag = TAG
         // Add the request to the RequestQueue.
         requestQueue?.add(stringRequest)
     }
 
     fun get_last_pay_code(callback: VolleyCallback){
-        url += "/get_last_pay_code"
+        url = ip
+        url += "/Payment_service/get_last_pay_code"
         // Request a string response from the provided URL.
         val stringRequest = StringRequest(
             Request.Method.GET, url,
@@ -53,26 +56,43 @@ class Payment(
                     response-> Log.d("Service error",response.toString())
             }
         )
-        stringRequest.tag = "Service FMS"
+        stringRequest.tag = TAG
         // Add the request to the RequestQueue.
         requestQueue?.add(stringRequest)
     }
 
-    fun insert(callback: VolleyCallback){
-        url += "/insert_payment"
-        val jsonBody = JSONObject()
-        //jsonBody.put("")
+    fun insert(jsonBody: JSONObject, callback: VolleyCallback){
+        url = ip
+        url += "/Payment_service/insert_payment"
         // Request a string response from the provided URL.
-        val stringRequest = StringRequest(
+        val stringRequest = object : StringRequest(
             Request.Method.POST, url,
-            Response.Listener<String> { response ->
-                //val accounting = JSONArray(response)
+            Response.Listener<String> {response ->
                 callback.onSuccess(response)
             },
             Response.ErrorListener {
                     response-> Toast.makeText(context, "${response}", Toast.LENGTH_SHORT).show()
+            }){
+            override fun getBodyContentType(): String {
+                return "application/x-www-form-urlencoded; charset=UTF-8";
             }
-        )
+
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String> {
+                val params = HashMap<String, String>()
+                params["pay_code"] = jsonBody.getString("pay_code")
+                params["pay_date"] = jsonBody.getString("pay_date")
+                params["pay_receipt"] = jsonBody.getString("pay_receipt")
+                params["pay_receipt_code"] = jsonBody.getString("pay_receipt_code")
+                params["pay_receipt_date"] = jsonBody.getString("pay_receipt_date")
+                params["pay_amount"] = jsonBody.getInt("pay_amount").toString()
+                params["pay_detail"] = jsonBody.getString("pay_detail")
+                params["pay_desc_id"] = jsonBody.getInt("pay_desc_id").toString()
+                params["pay_bac_id"] = jsonBody.getInt("pay_bac_id").toString()
+                return params
+            }
+        }
+        requestQueue?.add(stringRequest)
     }
 
     fun getByRangeDate(callback: VolleyCallback){
