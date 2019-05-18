509798cbe1c72e7d0f716b6324ccab29c39e918b
59160215 <59160215@go.buu.ac.th>
2019-05-18 14:55:58 +0700

    make add service success
    # Conflicts:
    #	app/src/main/java/com/example/fms_app/Manage_base_data/Manage_description.kt
    #	app/src/main/java/com/example/fms_app/Service/Bank.kt
    #	app/src/main/java/com/example/fms_app/Service/Bank_account.kt
    #	app/src/main/java/com/example/fms_app/Service/Conifg_service.kt
    #	app/src/main/java/com/example/fms_app/Service/Description.kt

---
+++ app/src/main/java/com/example/fms_app/Manage_base_data/BankAccount_detail.kt
@@ -0,0 +1,13 @@
+package com.example.fms_app.Manage_base_data
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fms_app.R
+
+class BankAccount_detail : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bank_account_detail)
+    }
+}
---
+++ app/src/main/java/com/example/fms_app/Manage_base_data/Description_detail.kt
@@ -0,0 +1,13 @@
+package com.example.fms_app.Manage_base_data
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fms_app.R
+
+class Description_detail : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_description_detail)
+    }
+}
